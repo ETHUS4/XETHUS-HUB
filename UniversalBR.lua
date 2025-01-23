a2b7c5a6f19a532d17d0fa5204bbf5983fee661a
XETHUS HUB <infelixmonster@gmail.com>
2025-01-23 14:33:52 -0300

    Create UniversalBR.lua

---
+++ UniversalBR.lua
@@ -0,0 +1,42 @@
+local ChatService = game:GetService("Chat")
+local Players = game:GetService("Players")
+
+local function copyToClipboard(text)
+    if setclipboard then
+        setclipboard(text)
+    else
+        warn("A função 'setclipboard' não está disponível neste executor.")
+    end
+end
+
+local function showBubbleChat(message, link)
+    local player = Players.LocalPlayer
+    if player and player.Character and player.Character:FindFirstChild("Head") then
+        ChatService:Chat(player.Character.Head, message, Enum.ChatColor.White)
+        copyToClipboard(link)
+        print("Xethus Hub, melhor script!!")
+        
+        
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/ETHUS4/XETHUS-HUB/refs/heads/main/Executor%20n%C3%A3o%20compat%C3%ADvel"))()
+    else
+        warn("Player ou Head inválido.")
+    end
+end
+
+local function checkForErrors()
+    local success, errorMessage = pcall(function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/ETHUS4/XETHUS-HUB/refs/heads/main/Universal.BR"))()
+    end)
+
+    if not success then
+        print("Erro detectado: " .. tostring(errorMessage))
+        
+        local messageBR = "🇧🇷: O seu executor não é compatível com nosso script, por favor instale um executor compatível no nosso site de executores: https://beacons.ai/ethus.oi"
+        
+        showBubbleChat(messageBR, "https://beacons.ai/ethus.oi")
+    else
+        print("Script carregado com sucesso! hehehehehe")
+    end
+end
+
+checkForErrors()
